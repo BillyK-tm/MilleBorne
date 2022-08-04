b6221bd6db590d0c8e94f34b2b15953c55a4b375
billyK <billy.kershaw@thinkmoney.co.uk>
2022-08-04 20:19:40 +0100

    updated calcs

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-const isWinner = false;
+let newGame = false;
 const safetyCard = 100;
 const fourSaftyCards = 400;
 const coupForre = 300;
@@ -8,8 +8,6 @@ const safeTrip = 300;
 const roundWinner = 500;
 const noTwohundreds = 300;
 const winningScore = 5000;
-let team1GameScores = [];
-let team2GameScores = [];
 let roundCount = 1;
 
 let team1Scores = {
@@ -40,7 +38,8 @@ const team1Name = document.getElementById("team1Name");
 const team1CalcScore = document.getElementById("team1CalcScore_btn");
 const team1Score = document.getElementById("team1Score");
 const team1RoundWinner = document.getElementById("team1RoundWin");
-const team1TwoHundredsUsed = document.getElementById("team1No200");
+const team1TwoHundredsUsedYes = document.getElementById("team1No200Yes");
+const team1TwoHundredsUsedNo = document.getElementById("team1No200YNo");
 const showTeam1Scores = document.getElementById("showScoresTeam1");
 const t1Scores = document.getElementById("t1Score");
 const team1NumCoupForres = document.getElementById("team1CoupForres");
@@ -48,37 +47,43 @@ const team1NumSafeties = document.getElementById("team1Safeties");
 const Team1GameScore = document.getElementById("team1GameScore");
 const roundScores = document.getElementById("roundScores");
 
-team1TwoHundredsUsed.disabled = true;
+const team2Name = document.getElementById("team2Name");
+const team2Score = document.getElementById("team2Score");
+const team2RoundWinner = document.getElementById("team2RoundWin");
+const team2TwoHundredsUsedYes = document.getElementById("team2No200Yes");
+const team2TwoHundredsUsedNo = document.getElementById("team2No200YNo");
+const showTeam2Scores = document.getElementById("showScoresTeam2");
+const t2Scores = document.getElementById("t2Score");
+const team2NumCoupForres = document.getElementById("team2CoupForres");
+const team2NumSafeties = document.getElementById("team2Safeties");
+const Team2GameScore = document.getElementById("team2GameScore");
+
 nextRound_btn.disabled = true;
-// team2TwoHundredsUsed.disabled = true
 
 team1Name.addEventListener("change", function () {
   team1Scores.teamName = team1Name.value;
 });
 team1Score.addEventListener("change", function (e) {
   team1Scores.roundScore = parseInt(team1Score.value);
+  console.log(team1Scores);
   calcScores(team1Scores);
 });
 team1RoundWinner.addEventListener("click", function () {
-  console.log("rnd winner");
   let ischecked = team1RoundWinner.checked;
   if (ischecked === true) {
     team1Scores.roundWinner = roundWinner;
-    team1TwoHundredsUsed.disabled = false;
   } else {
     team1Scores.roundWinner = 0;
-    team1TwoHundredsUsed.disabled = true;
-    team1TwoHundredsUsed.checked = false;
     team1Scores.noTwoHundreds = 0;
   }
   calcScores(team1Scores);
 });
-team1TwoHundredsUsed.addEventListener("click", function () {
-  let ischecked = team1TwoHundredsUsed.checked;
-  if (ischecked === false) {
-    team1Scores.noTwoHundreds = noTwohundreds;
-  } else {
+team1TwoHundredsUsedYes.addEventListener("click", function () {
+  let ischecked = team1TwoHundredsUsedYes.checked;
+  if (ischecked === true) {
     team1Scores.noTwoHundreds = 0;
+  } else {
+    team1Scores.noTwoHundreds = noTwohundreds;
   }
   calcScores(team1Scores);
 });
@@ -91,6 +96,7 @@ team1NumCoupForres.addEventListener("change", function () {
   calcScores(team1Scores);
 });
 team1NumSafeties.addEventListener("change", function () {
+  console.log(safetyCard);
   if (parseInt(team1NumSafeties.value) === 0) {
     team1Scores.safetyCards = 0;
   } else {
@@ -98,7 +104,51 @@ team1NumSafeties.addEventListener("change", function () {
   }
   calcScores(team1Scores);
 });
+// TEAM TWO
+team2Name.addEventListener("change", function () {
+  team2Scores.teamName = team2Name.value;
+});
+team2Score.addEventListener("change", function (e) {
+  team2Scores.roundScore = parseInt(team2Score.value);
+  calcScores(team2Scores);
+});
+team2RoundWinner.addEventListener("click", function () {
+  let ischecked = team2RoundWinner.checked;
+  if (ischecked === true) {
+    team2Scores.roundWinner = roundWinner;
+  } else {
+    team2Scores.roundWinner = 0;
+    team2Scores.noTwoHundreds = 0;
+  }
+  calcScores(team2Scores);
+});
+team2TwoHundredsUsedYes.addEventListener("click", function () {
+  let ischecked = team2TwoHundredsUsedYes.checked;
+  if (ischecked === true) {
+    team2Scores.noTwoHundreds = 0;
+  } else {
+    team2Scores.noTwoHundreds = noTwohundreds;
+  }
+  calcScores(team2Scores);
+});
+team2NumCoupForres.addEventListener("change", function () {
+  if (parseInt(team2NumCoupForres.value) === 0) {
+    team2Scores.coupForres = 0;
+  } else {
+    team2Scores.coupForres = parseInt(team2NumCoupForres.value) * coupForre;
+  }
+  calcScores(team2Scores);
+});
+team2NumSafeties.addEventListener("change", function () {
+  if (parseInt(team2NumSafeties.value) === 0) {
+    team2Scores.safetyCards = 0;
+  } else {
+    team2Scores.safetyCards = parseInt(team2NumSafeties.value) * safetyCard;
+  }
+  calcScores(team2Scores);
+});
 
+//FUNCTIONS
 function clearFields(team) {
   if (team.teamNumber === 1) {
     team1Scores.innerHTML = `round score: `;
@@ -106,61 +156,71 @@ function clearFields(team) {
     team1NumCoupForres.value = 0;
     team1NumSafeties.value = 0;
     team1RoundWinner.checked = false;
-    team1TwoHundredsUsed.disabled = true;
-    team1TwoHundredsUsed.checked = false;
+    // team1TwoHundredsUsed.disabled = true;
+    team1TwoHundredsUsedYes.checked = false;
     showTeam1Scores.innerHTML = "Round Scores: 0";
+    // team1GameScores.clearFields;
   } else {
     //Clear team 2
+    team2Scores.innerHTML = `round score: `;
+    team2Score.value = 0;
+    team2NumCoupForres.value = 0;
+    team2NumSafeties.value = 0;
+    team2RoundWinner.checked = false;
+    // team1TwoHundredsUsed.disabled = true;
+    team2TwoHundredsUsedYes.checked = false;
+    showTeam2Scores.innerHTML = "Round Scores: 0";
+    // team2GameScores.clearFields;
   }
+  clearObjects(team1Scores);
+  clearObjects(team2Scores);
 }
-function clearObjects() {
-  team1Scores = {
-    teamNumber: 1,
-    teamName: "Team1",
-    roundScore: 0,
-    roundWinner: 0,
-    coupForres: 0,
-    safetyCards: 0,
-    noTwoHundreds: 0,
-    totalRoundScore: 0,
-    totalGameScore: 0,
-  };
+function clearObjects(team) {
+  teamNumber = team.teamNumber;
+  team.teamNumber = teamNumber;
+  team.teamName = "Team" + teamNumber;
+  team.roundScore = 0;
+  team.roundWinner = 0;
+  team.coupForres = 0;
+  team.safetyCards = 0;
+  team.noTwoHundreds = 0;
+  team.totalRoundScore = 0;
 
-  team2Scores = {
-    teamNumber: 2,
-    teamName: "Team2",
-    roundScore: 0,
-    roundWinner: 0,
-    coupForres: 0,
-    safetyCards: 0,
-    noTwoHundreds: 0,
-    totalRoundScore: 0,
-    totalGameScore: 0,
-  };
-  roundScores.innerHTML = "";
+  if (newGame === true) {
+    team.totalGameScore = 0;
+  }
 }
 
 newGame_btn.addEventListener("click", function () {
   clearFields(team1Scores);
   clearFields(team2Scores);
+  clearObjects(team1Scores);
+  clearObjects(team2Scores);
   nextRound_btn.disabled = false;
-  roundScores.innerHTML = "";
+  newGame = true;
+  showTeam1Scores.innerHTML = "Round Scores:";
+  showTeam2Scores.innerHTML = "Round Scores:";
+  Team1GameScore.innerHTML = "Running Total:";
+  Team2GameScore.innerHTML = "Running Total:";
 });
 
 nextRound_btn.addEventListener("click", function () {
   team1Scores.totalGameScore += team1Scores.totalRoundScore;
-  team1Scores.innerHTML = `round score: 0`;
+  team2Scores.totalGameScore += team2Scores.totalRoundScore;
 
-  team1GameScores.push(team1Scores);
-  updateRoundScores(team1Scores);
+  Team1GameScore.innerHTML = `Running Total: ${team1Scores.totalGameScore}`;
+  Team2GameScore.innerHTML = `Running Total: ${team2Scores.totalGameScore}`;
+
   checkWinner();
   clearFields(team1Scores);
+  clearFields(team2Scores);
 });
 function checkWinner() {
   if (
     team1Scores.totalGameScore >= winningScore ||
     team2Scores.totalGameScore >= winningScore
   ) {
+    console.log("Check winner - in IF");
     if (team1Scores.totalGameScore > team2Scores.totalGameScore) {
       // player One run
       Team1GameScore.innerHTML = `Well done ${team1Scores.teamName} Your a Winner`;
@@ -173,39 +233,33 @@ function checkWinner() {
   } else {
     console.log("-");
   }
-
-  if (isWinner === false) {
-    // check who won
-  } else {
-    updateRoundScores();
-  }
 }
 function updateRoundScores(team) {
-  roundScores.innerHTML = "";
-  let listItems = "";
-  listItems += `
-    <table id="roundScores">
-    <tr>
-      <th>Round Number</th>
-      <th>Team</th>
-      <th>Score</th>
-      <th>Total Score</th>
-      
-  </tr>
-  <tr>
-  <td>${roundCount}</td>
-  <td>${team.teamName}</td>
-    <td>${team.totalRoundScore}</td>
-    <td>${team.totalGameScore}</td>
-  </tr>
-  <tr>
-  <td>${roundCount}</td>
-  <td>${team.teamName}</td>
-    <td>${team.totalRoundScore}</td>
-    <td>${team.totalGameScore}</td>
-  </tr>
-  `;
-  roundScores.innerHTML += listItems;
+  // roundScores.innerHTML = "";
+  // let listItems = "";
+  // listItems += `
+  //   <table id="roundScores">
+  //   <tr>
+  //     <th>Round Number</th>
+  //     <th>Team</th>
+  //     <th>Score</th>
+  //     <th>Total Score</th>
+  // </tr>
+  // <tr>
+  // <td>${roundCount}</td>
+  // <td>${team.teamName}</td>
+  //   <td>${team.totalRoundScore}</td>
+  //   <td>${team.totalGameScore}</td>
+  // </tr>
+  // <tr>
+  // <td>${roundCount}</td>
+  // <td>${team.teamName}</td>
+  //   <td>${team.totalRoundScore}</td>
+  //   <td>${team.totalGameScore}</td>
+  // </tr>
+  // `;
+  // roundScores.innerHTML += listItems;
+  // Team1GameScore.innerHTML = team.totalGameScore;
 }
 
 function calcScores(team) {
@@ -218,10 +272,11 @@ function calcScores(team) {
     team.noTwoHundreds;
 
   if (team.teamNumber === 2) {
-    player2Scores.innerHTML = `round score: ${team.totalRoundScore}`;
+    showTeam2Scores.innerHTML = `round score: ${team.totalRoundScore}`;
+    console.log("team 2 calc scores");
   } else {
     showTeam1Scores.innerHTML = `round score: ${team.totalRoundScore}`;
-
-    // t1Scores.innerHTML = `round score ${team.totalRoundScore}`;
+    console.log("team 1 calc scores");
+    // Team1GameScore.innerHTML = `Running Total: ${team.totalGameScore}`;
   }
 }
